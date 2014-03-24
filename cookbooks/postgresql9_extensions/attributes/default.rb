1a2ae33bd380f0a65b7130ec2841263ba212c6af
Tyler Poland <tyler.poland@gmail.com>
2014-03-24 16:15:25 -0400

    Updated postgres version attributes to be future proof against future major versions in the format of \d+_\d+

--- cookbooks/postgresql9_extensions/attributes/default.rb
+++ cookbooks/postgresql9_extensions/attributes/default.rb
@@ -3,8 +3,6 @@ default[:postgres_root] = "/db/postgresql/"
 
 if db_stack == "postgres9"
   default[:postgres_version] = "9.0"
-elsif db_stack == "postgres9_1"
-  default[:postgres_version] = "9.1"
-elsif db_stack == "postgres9_2"
-  default[:postgres_version] = "9.2"
+elsif db_stack =~ /postgres\d+_\d+/
+  default[:postgres_version] = db_stack.gsub('postgres','').gsub('_','.')
 end
